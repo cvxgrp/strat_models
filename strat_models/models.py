785d8101ad6df1b2bfa3723a25257f04f0923ecf
Jonathan Tuck <jonathantuck1@gmail.com>
2019-11-18 12:03:22 -0800

    update on models

--- strat_models/models.py
+++ strat_models/models.py
@@ -150,8 +150,7 @@ class StratifiedModel:
 			self.K = L.shape[0]
 			return L
 		else:
-			# eigvals, Q_tilde = scipy.sparse.linalg.eigs(nx.laplacian_matrix(self.G), which="SM", k=num_eigen+1, return_eigenvectors=True)
-			eigvals, Q_tilde = np.linalg.eigh(nx.laplacian_matrix(self.G).A)
+			eigvals, Q_tilde = np.linalg.eigh(nx.laplacian_matrix(self.G).toarray())
 			eigvals = eigvals[:num_eigen]
 			Q_tilde = Q_tilde[:,:num_eigen]
 			self.nodelist = self.G.nodes()
